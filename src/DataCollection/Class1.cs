ce00bcc05d99ac53dff4c539942cbecf326c861e
Peter Asamoah <peter.asamoah7@outlook.com>
2021-02-12 20:59:50 +0000

    Add project files.

---
+++ src/DataCollection/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataCollection
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Search/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Search
+{
+    public class Class1
+    {
+    }
+}
